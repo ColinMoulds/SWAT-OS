aa8ce7f0ecd3813a28333ed45771d194ed8309bb
Colin Moulds <colin.moulds1998@gmail.com>
2017-10-29 03:09:35 +0000

    Update kernal.cpp
    fixing typo's

--- Source/kernal.cpp
+++ Source/kernal.cpp
@@ -100,6 +100,6 @@ void terminal_writestring(const char* data)
 void kernel_main()
 {
 terminal_initialize();
-terminal_writestring("wellcome to my first operating system!");
+terminal_writestring("welcome to my first operating system!");
 for(;;);
 }
